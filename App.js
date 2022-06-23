825f5f705226dad44fd5698b1ae8e8a8d4f42056
rpi <95402257+rpi-esp32@users.noreply.github.com>
2022-06-23 15:47:01 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+//import {Test} from './Test.js';
+
+import ReactDOM from "react-dom";
+
+
+function App(){
+  return (<div><h1>FILE</h1></div>);
+  
+}
+
+
+
+export default App;
